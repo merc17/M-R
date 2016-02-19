c24c8f05bca9fa932c94109c63dfe4594f3646de
merc17 <andreski33@icloud.com>
2016-02-19 00:07:14 -0600

    first commit

---
+++ FireAnalysisTool/src/fhx/FireHistoryFile.java
@@ -0,0 +1,59 @@
+/**
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+*/
+
+package fhx;
+
+import java.util.List;
+
+/**
+ 
+ @author gerhardyz
+ */
+public class FireHistoryFile 
+{
+   private String file_name;
+   
+   private String name_of_site;
+   private String site_code;
+   private String collection_date;
+   private String collectors;
+   private String crossdaters;
+   private String number_samples;
+   private String species_name;
+   private String common_name;
+   private String habitat_type;
+   private String country;
+   private String state;
+   private String county;
+   private String park_monument;
+   private String national_forest;
+   private String ranger_district;
+   private String township;
+   private String range;
+   private String section;
+   private String quarter_section; 
+   private String utm_easting;
+   private String utm_northing;
+   private String latitude;
+   private String longitude;
+   private String topographic_map;
+   private String lowest_elev;
+   private String highest_elev;
+   private String slope;
+   private String aspect;
+   private String area_sampled;
+   private String substrate_type;
+   private String comment;
+   private int begin_year;
+   private int number_of_samples;
+   private int id_length;
+   
+   private List<Series> series;
+   
+   FireHistoryFile(){
+      
+   }
+}
