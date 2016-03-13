99f523bf31c1aab8c17d02005b14297c581fd2aa
Sean Morgan <cmo@cmo.h>
2016-03-13 14:28:58 -0500

    moved domain classed out of ui pack

---
+++ FireAnalysisTool/src/firehistory/Series.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firehistory;
+
+import java.util.List;
+
+/**
+ *
+ * @author cmo
+ */
+public class Series {
+
+    private String series_id;
+    private List<Character> record;
+
+    Series() {
+
+    }
+}
