11123d2d9e5830b96943ec2cfe6f920ff8294ba4
Sean Morgan <cmo@cmo.h>
2016-03-13 15:19:56 -0500

    Setting up cmd operations

---
+++ FireAnalysisTool/src/FHX2_BrennenSucks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JFrame;
+import fhx.mainFrame;
+
+/**
+ *
+ * @author andreskis
+ */
+public class FHX2_BrennenSucks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //if
+        JFrame f = new mainFrame();
+        f.setVisible(true);
+
+    }
+
+}
