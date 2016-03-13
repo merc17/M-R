618d80a615fb21c7b0e952443f1aee577d610432
cmo8 <cmo.uwp.2010@gmail.com>
2016-03-13 16:43:54 -0500

    working on commandline

--- FireAnalysisTool/src/FHX2_BrennenSucks.java
+++ FireAnalysisTool/src/FHX2_BrennenSucks.java
@@ -17,10 +17,16 @@ public class FHX2_BrennenSucks {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //if
-        JFrame f = new mainFrame();
-        f.setVisible(true);
-
+        if (args.length == 0 || args[0].compareTo("gui") == 0) {
+            System.out.println("Starting GUI...");
+            JFrame f = new mainFrame();
+            f.setVisible(true);
+        } else if (args[0].compareTo("h") == 0 || args[0].compareTo("help") == 0 || args[0].compareTo("man") == 0) {
+            System.out.println("Fire History Man Page");
+        } else if (args[0].compareTo("d") == 0 || args[0].compareTo("display") == 0) {
+            System.out.print("Display Fire History");
+        } else if (args[0].compareTo("c") == 0 || args[0].compareTo("create") == 0){
+            System.out.print("Create Fire History");
+        }
     }
-
 }
